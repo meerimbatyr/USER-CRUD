71c6adccfec2921c8a6910c7929f6a118806bc9d
Ivan Gilyov <gilev.joyyouth@gmail.com>
2022-10-07 17:03:23 -0700

    book link component

--- src/components/Book/index.js
+++ src/components/Book/index.js
@@ -1,20 +1,31 @@
 import React from 'react';
 import "./Book.css"
 import { Button, Table } from "react-bootstrap";
+import { useLocation } from "react-router-dom"
+import { useEffect } from "react"
 
 function Book(props) {
+
+    
+    const { state } = useLocation();
+    useEffect=(() => {
+        console.log(state)
+
+
+    },[])
+
     return (
 
         <div className='book'>
             <div>
-                <h2>Title: {}</h2>
-                <img src="https://www.imgacademy.com/sites/default/files/2022-07/img-homepage-meta.jpg" alt="" />
+                <h2>Title: {state.title}</h2>
+                <img src={state.cover} alt="" />
 
             </div>
             <div>
-                <p><span>Author:</span> Lorem, ipsum dolor.</p>
-                <p><span>Genre:</span> Lorem.</p>
-                <p><span>Description:</span> Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam, porro!</p>
+                <p><span>Author:</span> {state.author}</p>
+                <p><span>Genre:</span> {state.genre}</p>
+                <p><span>Description:</span>{state.description}</p>
                 <Button variant="primary">
                   Update
                 </Button>
